f03e647adb08887af5640ff0d26a05c1b416e899
Fred Lahde <fredlahde@gmail.com>
2017-11-23 23:04:31 +0100

    config singleton

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -3,28 +3,40 @@ package config;
 
 import helper.IOHelper;
 import lombok.Cleanup;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j;
 
 import java.io.*;
 import java.util.Properties;
 
+@Log4j
 public class Config {
 
-    private static File configFile = new java.io.File(System.getProperty("user.home") + "/.push/push.properties");
-    private static File configFileDir = new java.io.File(System.getProperty("user.home") + "/.push");
+    @Getter
+    private static Config instance = new Config();
+
+    private File configFile = new java.io.File(System.getProperty("user.home") + "/.push/push.properties");
+    private File configFileDir = new java.io.File(System.getProperty("user.home") + "/.push");
     private Properties properties;
 
-    public Config() throws IOException {
-        @Cleanup InputStream fsin = this.getClass().getClassLoader().getResourceAsStream("push.properties");
-        if (!configFile.exists()) {
-            configFileDir.mkdirs();
-            @Cleanup FileOutputStream fout = new FileOutputStream(configFile);
-            configFile.createNewFile();
-            IOHelper.copy(fsin, fout);
-            fout.flush();
+    public Config() {
+        try {
+            @Cleanup InputStream fsin = this.getClass().getClassLoader().getResourceAsStream("push.properties");
+            if (!configFile.exists()) {
+                configFileDir.mkdirs();
+                @Cleanup FileOutputStream fout = new FileOutputStream(configFile);
+                configFile.createNewFile();
+                IOHelper.copy(fsin, fout);
+                fout.flush();
+            }
+            this.properties = new Properties();
+            @Cleanup FileInputStream sysIn = new FileInputStream(configFile);
+            properties.load(sysIn);
+        } catch (IOException e) {
+            log.error(e.getLocalizedMessage());
+            e.printStackTrace();
+            System.exit(1);
         }
-        this.properties = new Properties();
-        @Cleanup FileInputStream sysIn = new FileInputStream(configFile);
-        properties.load(sysIn);
 
     }
 
