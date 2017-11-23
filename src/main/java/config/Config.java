f8cc33b1673718351e46dd2cf1935fa8b9b6d649
Fred Lahde <fredlahde@gmail.com>
2017-11-23 21:51:05 +0100

    start v2, lombok

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -2,9 +2,9 @@ package config;
 
 
 import helper.IOHelper;
+import lombok.Cleanup;
 
 import java.io.*;
-import java.util.Optional;
 import java.util.Properties;
 
 public class Config {
@@ -14,20 +14,18 @@ public class Config {
     private Properties properties;
 
     public Config() throws IOException {
-        try (InputStream fsin = this.getClass().getClassLoader().getResourceAsStream("push.properties")) {
-            if (!configFile.exists()) {
-                configFileDir.mkdirs();
-                try (FileOutputStream fout = new FileOutputStream(configFile)) {
-                    configFile.createNewFile();
-                    IOHelper.copy(fsin, fout);
-                    fout.flush();
-                }
-            }
+        @Cleanup InputStream fsin = this.getClass().getClassLoader().getResourceAsStream("push.properties");
+        if (!configFile.exists()) {
+            configFileDir.mkdirs();
+            @Cleanup FileOutputStream fout = new FileOutputStream(configFile);
+            configFile.createNewFile();
+            IOHelper.copy(fsin, fout);
+            fout.flush();
         }
         this.properties = new Properties();
-        try (FileInputStream sysIn = new FileInputStream(configFile)) {
-            properties.load(sysIn);
-        }
+        @Cleanup FileInputStream sysIn = new FileInputStream(configFile);
+        properties.load(sysIn);
+
     }
 
     public Properties getProperties() {
